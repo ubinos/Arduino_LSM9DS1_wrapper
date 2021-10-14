0bd900e30b0ea0435759793cdb2ec71771e77af5
Sungho Park <siminsungho@gmail.com>
2021-10-14 10:41:38 +0900

    refactoring

--- config/adafruit_lsm9ds1.cmake
+++ config/adafruit_lsm9ds1.cmake
@@ -1,3 +1,9 @@
+#
+# Copyright (c) 2021 Sung Ho Park and CSOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 set(INCLUDE__ARDUINO_LSM9DS1 TRUE)
 
 set_cache_default(ARDUINO_LSM9DS1__BASE_DIR "${PROJECT_LIBRARY_DIR}/Arduino_LSM9DS1" STRING "Arduino LSM9DS1 project base dir")
--- source/sources.cmake
+++ source/sources.cmake
@@ -1,3 +1,9 @@
+#
+# Copyright (c) 2021 Sung Ho Park and CSOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 if(INCLUDE__ARDUINO_LSM9DS1)
 
 get_filename_component(_tmp_source_dir "${ARDUINO_LSM9DS1__BASE_DIR}" ABSOLUTE)

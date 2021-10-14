174a9e21eee2cbc2bdb449a1266d45aab2d8a424
Sungho Park <siminsungho@gmail.com>
2021-10-14 10:30:21 +0900

    add ubinos app, arduino_lsm9ds1_SimpleAccelerometer_arduinonano33ble

---
+++ app/arduino_lsm9ds1_SimpleAccelerometer/appmain.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2021 Sung Ho Park and CSOS
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <ubinos.h>
+
+
+#if (INCLUDE__APP__arduino_lsm9ds1_SimpleAccelerometer == 1)
+
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include <Arduino.h>
+
+void printValues();
+
+#include "ino/SimpleAccelerometer.ino"
+
+static void arduino_func(void * arg);
+
+int appmain(int argc, char * argv[]) {
+	int r;
+	(void) r;
+
+	r = task_create(NULL, arduino_func, NULL, task_getlowestpriority(), 0, "arduino");
+	assert(r == 0);
+
+	ubik_comp_start();
+
+	return 0;
+}
+
+static void arduino_func(void * arg) {
+	printf("\n\n\n");
+	printf("================================================================================\n");
+	printf("arduino_lsm9ds1_SimpleAccelerometer (build time: %s %s)\n", __TIME__, __DATE__);
+	printf("================================================================================\n");
+    printf("\n");
+
+    initVariant();
+    setup();
+    for (unsigned int i = 0; ; i++) {
+        loop();
+    }
+}
+
+
+#endif /* (INCLUDE__APP__arduino_lsm9ds1_SimpleAccelerometer == 1) */
+
---
+++ app/arduino_lsm9ds1_SimpleAccelerometer_arduinonano33ble.cmake
@@ -0,0 +1,22 @@
+#
+# Copyright (c) 2021 Sung Ho Park and CSOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# {ubinos_config_type: [buildable, cmake, app]}
+
+set(INCLUDE__APP TRUE)
+set(APP__NAME "arduino_lsm9ds1_SimpleAccelerometer")
+
+include(${PROJECT_LIBRARY_DIR}/ArduinoCore-API_wrapper/config/arduinocore_api_arduinonano33ble.cmake)
+include(${PROJECT_LIBRARY_DIR}/Arduino_LSM9DS1_wrapper/config/adafruit_lsm9ds1.cmake)
+
+get_filename_component(_tmp_source_dir "${CMAKE_CURRENT_LIST_DIR}/${APP__NAME}" ABSOLUTE)
+file(GLOB_RECURSE _tmp_sources
+    "${_tmp_source_dir}/*.c"
+    "${_tmp_source_dir}/*.cpp"
+    "${_tmp_source_dir}/*.S"
+    "${_tmp_source_dir}/*.s")
+set(PROJECT_APP_SOURCES ${PROJECT_APP_SOURCES} ${_tmp_sources})
+

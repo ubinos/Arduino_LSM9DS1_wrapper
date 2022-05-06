faadcb80134b92702ca53b03c880a0fa7fb51e1d
Sungho Park <siminsungho@gmail.com>
2022-05-06 17:08:21 +0900

    add app config, arduino_lsm9ds1_SimpleAccelerometer_arduinonano33ble_bleuart, arduino_lsm9ds1_SimpleAccelerometer_adafruitnrf52840e, arduino_lsm9ds1_SimpleAccelerometer_adafruitnrf52840e_bleuart.

--- make/batch.mk
+++ make/batch.mk
@@ -17,6 +17,26 @@
 	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=arduino_lsm9ds1_SimpleAccelerometer_arduinonano33ble
 	@echo ""
 	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=arduino_lsm9ds1_SimpleAccelerometer_arduinonano33ble_bleuart
+	@echo ""
+	@echo ""
+
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=arduino_lsm9ds1_SimpleAccelerometer_adafruitnrf52840e
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=arduino_lsm9ds1_SimpleAccelerometer_adafruitnrf52840e_bleuart
+	@echo ""
+	@echo ""
 
 
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"

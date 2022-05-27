b229139bcf7b77ab2e312351170fc6a60914d151
Sungho Park <siminsungho@gmail.com>
2022-05-27 17:21:31 +0900

    add ubinos app config, arduino_lsm9ds1_SimpleAccelerometer_nrf52840dk, arduino_lsm9ds1_SimpleAccelerometer_nrf52840dk_bleuart, arduino_lsm9ds1_SimpleAccelerometer_nucleof207zg.

---
+++ app/arduino_lsm9ds1_SimpleAccelerometer_nrf52840dk.cmake
@@ -0,0 +1,26 @@
+#
+# Copyright (c) 2022 Sung Ho Park and CSOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# ubinos_config_info {"name_base": "arduino_lsm9ds1_SimpleAccelerometer", "build_type": "cmake_ubinos", "app": true}
+
+include(${PROJECT_LIBRARY_DIR}/ArduinoCore-API_wrapper/config/arduinocore_api_nrf52840dk.cmake)
+include(${PROJECT_LIBRARY_DIR}/Arduino_LSM9DS1_wrapper/config/adafruit_lsm9ds1.cmake)
+
+####
+
+set(INCLUDE__APP TRUE)
+set(APP__NAME "arduino_lsm9ds1_SimpleAccelerometer")
+
+get_filename_component(_tmp_source_dir "${CMAKE_CURRENT_LIST_DIR}/${APP__NAME}" ABSOLUTE)
+
+file(GLOB_RECURSE _tmp_sources
+    "${_tmp_source_dir}/*.c"
+    "${_tmp_source_dir}/*.cpp"
+    "${_tmp_source_dir}/*.cc"
+    "${_tmp_source_dir}/*.S")
+
+set(PROJECT_APP_SOURCES ${PROJECT_APP_SOURCES} ${_tmp_sources})
+
--- make/batch.mk
+++ make/batch.mk
@@ -41,6 +41,28 @@
 
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=arduino_lsm9ds1_SimpleAccelerometer_nrf52840dk
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=arduino_lsm9ds1_SimpleAccelerometer_nrf52840dk_bleuart
+	@echo ""
+	@echo ""
+
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=arduino_lsm9ds1_SimpleAccelerometer_nucleof207zg
+	@echo ""
+	@echo ""
+
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
 
 ###############################################################################
 
